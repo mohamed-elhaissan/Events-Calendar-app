a0c364afaedcd41bdc460cb2a1b69ea771fc7ee0
mohamed <mhalwasoffice@gmail.com>
2024-06-06 12:07:44 +0100

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("test");
+const container = document.querySelector(".container");
+for(let i=1;i<=30;i++){
+    let div = document.createElement("div");
+    div.textContent = i;
+    div.classList.add("day");
+    container.appendChild(div)
+
+}
