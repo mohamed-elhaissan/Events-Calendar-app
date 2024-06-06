14ba6286f56b22a805a2f265a2f765d0fba41396
mohamed <mhalwasoffice@gmail.com>
2024-06-06 16:03:04 +0100

    bug

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const container = document.querySelector(".container");
-for(let i=1;i<=30;i++){
+
+
+
+
+const date = new Date();
+const month = date.getMonth();
+const DaysNumber = new Date(date.getFullYear(), month + 1, 0).getDate();
+console.log(DaysNumber);
+
+
+for (let i = 1; i <= DaysNumber; i++) {
     let div = document.createElement("div");
     div.textContent = i;
     div.classList.add("day");
-    container.appendChild(div)
+    container.appendChild(div);
+    
 
 }
