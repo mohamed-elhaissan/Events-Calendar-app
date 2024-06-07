a763a0681c567785eefc222251df95b5d320a71f
mohamed <mhalwasoffice@gmail.com>
2024-06-07 22:33:13 +0100

    updateing

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const confirmtbn = document.getElementById("confirm");
 const icon = document.querySelector("ion-icon");
 const input = document.querySelector("#taskinput");
 const divalert = document.querySelector(".sorryalert");
+const span = document.querySelectorAll('.day span');
 // block
 
 icon.addEventListener("click", function () {
@@ -13,6 +14,7 @@ icon.addEventListener("click", function () {
 
 cancel.addEventListener("click", function () {
     task.style.display = 'none';
+    input.value = ''
 })
 const date = new Date();
 const month = date.getMonth();
@@ -20,7 +22,9 @@ const DaysNumber = new Date(date.getFullYear(), month + 1, 0).getDate();
 
 for (let i = 1; i <= DaysNumber; i++) {
     let div = document.createElement("div");
-    div.textContent = i;
+    let h3 = document.createElement('h3');
+    h3.textContent = i;
+    div.appendChild(h3)
     div.classList.add("day");
     container.appendChild(div);
 }
@@ -30,22 +34,27 @@ div.forEach((element) => {
     element.addEventListener("click", function () {
         task.style.display = 'block';
         document.querySelector("input").focus();
-        
+
     })
 })
 for (let index = 0; index < div.length; index++) {
-    div[index].addEventListener('click',function () { 
-        confirmtbn.onclick = function () { 
+    div[index].addEventListener('click', function () {
+        confirmtbn.onclick = function () {
             if (input.value.length == 0) {
-                divalert.style.display = 'block';
-                setTimeout(() => {
-                    divalert.style.opacity = 1;
-                }, 1000);
-                setTimeout(() => {
-                    divalert.style.display = 'none';
-                }, 1200);
+                input.style.borderColor = 'red';
+                input.placeholder = 'enter some value please .... !';
+                input.addEventListener('input', function () {
+                    if (input.value.length != 0) {
+                        input.style.borderColor = 'green';
+                    }
+                })
             }
-         }
-     })
-    
+            let span = document.createElement('span');
+            span.textContent = input.value;
+            div[index].appendChild(span);
+            input.value = '';
+            task.style.display = '';
+        }
+    })
+
 }
